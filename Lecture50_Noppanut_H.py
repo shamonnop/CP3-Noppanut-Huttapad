f577389d162efb056ebae08cb3a1f3099d470e26
shamonnop <nop.hut@gmail.com>
2022-02-10 18:34:10 +0700

    add Lecture50

---
+++ Lecture50_Noppanut_H.py
@@ -0,0 +1,24 @@
+def addNumber(x, y):
+	print("x + y = ", x+y)
+
+
+def subNumber(x, y):
+	print("x - y = ", x - y)
+
+
+def mulNumber(x, y):
+	print("x * y = ", x * y)
+
+
+def divNumber(x, y):
+	print("x / y = ", x/y)
+
+
+x = int(input("Enter Number : "))
+y = int(input("Enter Number : "))
+addNumber(x, y)
+subNumber(x, y)
+mulNumber(x, y)
+subNumber(x, y)
+
+
